24ff285df601038ea3edff4a6471b84cc141885e
Kurbass <Kurbass@DESKTOP-QN1JGDF>
2022-11-03 14:20:23 +0200

    Добавьте файлы проекта.

---
+++ ExamWork/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ExamWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 415);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(138, 415);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ExamWork/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ExamWork
+{
+    public partial class Form1 : Form
+    {
+        int ProgressBarCount = 0;
+        Random rand = new Random();
+        List<ProgressBar> ProgressBarList = new List<ProgressBar>();
+        List<Label> LabelList = new List<Label>();
+        List<Thread> ThreadList = new List<Thread>();
+        Semaphore semaphore = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            semaphore = new Semaphore(3, 3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (ProgressBarCount < 10)
+            {
+                ProgressBar p = new ProgressBar();
+                p.Location = new Point(MousePosition.X, MousePosition.Y);
+                p.Name = "p" + ProgressBarCount + rand.Next(100, 200);
+                p.Step = 1;
+
+                Label l = new Label();
+                l.Text = rand.Next(1, Convert.ToInt32(numericUpDown1.Value)).ToString();
+                l.Location = new Point(p.Location.X + 102, p.Location.Y);
+                l.Name = "l" + ProgressBarCount + rand.Next(100, 200);
+                p.Maximum = Convert.ToInt32(l.Text);
+                p.Minimum = 0;
+
+                this.Controls.Add(p);
+                this.Controls.Add(l);
+                ProgressBarList.Add(p);
+                LabelList.Add(l);
+                ThreadList.Add(new Thread(new ParameterizedThreadStart(AsyncWork)));
+                ProgressBarCount++;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                timer1.Enabled = false;
+            else
+                timer1.Enabled = true;
+        }
+
+        private void AsyncWork(object index)
+        {
+            int _index = (int)index;
+            ProgressBar tmpProgressBar = ProgressBarList[_index];
+            Label tmpLabel = LabelList[_index];
+            Thread tmpThread = ThreadList[_index];
+            semaphore.WaitOne();
+            for (int i = 0; i < ProgressBarList.Find(x => x.Name.Contains(tmpProgressBar.Name)).Maximum; i++)
+            {
+                Thread.Sleep(1000);
+                this.Invoke((MethodInvoker)delegate () 
+                { 
+                    ProgressBarList.Find(x => x.Name.Contains(tmpProgressBar.Name)).PerformStep(); 
+                });
+            }
+            this.Invoke((MethodInvoker)delegate () 
+            { 
+                this.Controls.Remove(ProgressBarList.Find(x => x.Name.Contains(tmpProgressBar.Name))); 
+            });
+            this.Invoke((MethodInvoker)delegate () 
+            { 
+                this.Controls.Remove(LabelList.Find(x => x.Name.Contains(tmpLabel.Name))); 
+            });
+
+            ProgressBarList.Remove(tmpProgressBar);
+            LabelList.Remove(tmpLabel);
+            ThreadList.Remove(tmpThread);
+            ProgressBarCount--;
+            semaphore.Release();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (ThreadList.Count == 0)
+                return;
+
+            for (int i = 0; i < ThreadList.Count; i++)
+            {
+                if (ThreadList[i].IsAlive != true)
+                    ThreadList[i].Start(i);
+            }    
+        }
+    }
+}
